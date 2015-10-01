2a65aa3f1af6f654e21a067377a7eb0279c41657
bianet13 <bia.macedo13@gmail.com>
2015-10-01 13:10:47 -0300

    init commit

--- Shake It/ViewController.swift
+++ Shake It/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    var sounds = ["pop", "crash", "jump", "beep", "screech", "bomb", "scream", "boing", "fall"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.MotionShake {
+            
+            let randomNumber = Int(arc4random_uniform(UInt32(sounds.count)))
+            
+            let fileLocation = NSBundle.mainBundle().pathForResource(sounds[randomNumber], ofType: "mp3")
+            
+            do{
+                
+                try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: fileLocation!))
+                
+                player.play()
+                
+            } catch {
+                // Exception Handling
+            }
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
